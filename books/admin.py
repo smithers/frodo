b0a85f819f226de5189a9cddf46a5b3eada3b653
smithers <czarniak@gmail.com>
2026-01-01 17:40:01 -0500

    show date user joined

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import Author, Book, UserFavoriteBook
 
 @admin.register(Author)
@@ -14,3 +17,12 @@ class BookAdmin(admin.ModelAdmin):
 class FavoriteBookAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'created_at')
     list_filter = ('created_at',)
+
+
+# Show user creation date in the Users admin list
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
